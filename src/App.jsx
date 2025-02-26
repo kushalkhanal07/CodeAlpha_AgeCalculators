49b5b36f009832a9fd26abfda2a2871bd9ec55fd
kushalkhanal07 <137860642+kushalkhanal07@users.noreply.github.com>
2025-02-26 20:53:02 +0545

    message changed

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
   function handleChange(e) {
     const dates = e.target.value;
     const value = dates.split("-");
-    const date = new Date();
+    console.log*value
     const FinaleDate = date.getFullYear() - (+value[0]) - 1;
     const FinalMonth = 12 - (+value[1]) + 1;
     setAge({ FinaleDate, FinalMonth });
